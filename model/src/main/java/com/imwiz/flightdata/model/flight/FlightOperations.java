80f9a77038c3e81223ae03bbc305c96716879fb5
falenn <dev@imwiz.com>
2023-03-30 21:33:39 -0400

    Updated poms for Jackson / JSON support.

--- model/src/main/java/com/imwiz/flightdata/model/flight/FlightOperations.java
+++ model/src/main/java/com/imwiz/flightdata/model/flight/FlightOperations.java
@@ -1,19 +1,34 @@
 package com.imwiz.flightdata.model.flight;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.jackson.Jacksonized;
 
 @Builder
 @Getter
 @ToString
+@Jacksonized()
 public class FlightOperations {
 
 	private UUID id;
+	
+	@Default
+	@JsonSerialize(using = ZonedDateTimeSerializer.class)
+	@JsonFormat(pattern = "YYYY-MM-DD HH:mm:ss z")
+	private ZonedDateTime createdDate = ZonedDateTime.of(LocalDateTime.now(),ZoneId.of("GMT"));
 	
 	private String flightName;
 	
